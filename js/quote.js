41c13b53e07118582c2328a4f29bb9173c87a542
white <exwhite@naver.com>
2023-01-18 00:07:06 +0900

    Update MP

--- js/quote.js
+++ js/quote.js
@@ -1,46 +1,46 @@
 const quotes = [
   {
     quote: "It does not matter how slowly you go as long as you do not stop.",
-    author: "-Confucius-",
+    author: "- Confucius -",
   },
   {
     quote: "To enhoy the rain bow, first enjoy the rain.",
-    author: "-Paulo Coelho-",
+    author: "- Paulo Coelho -",
   },
   {
     quote: "When it rains look for rainbows, when it's dark look for stars.",
-    author: "-Oscar Wilde-",
+    author: "- scar Wilde -",
   },
   {
     quote: "Life is unfair, get used to it.",
-    author: "-Bill Gates-",
+    author: "- Bill Gates -",
   },
   {
     quote: "Despite the forecast, live like it's spring.",
-    author: "-Lilly Pulitzer-",
+    author: "- Lilly Pulitzer -",
   },
   {
     quote: "The way to get started is to quit talking and begin doing.",
-    author: "-Walt Disney-",
+    author: "- Walt Disney -",
   },
   {
     quote:
       "If you don't get out of the box you've been raised in, you won't understand how nuch bigger the world is.",
-    author: "-Angelina Jolie-",
+    author: "- Angelina Jolie -",
   },
   {
     quote: "Keep your eyes on the stars and your feet on the ground.",
-    author: "-Theodore Roosevelt-",
+    author: "- Theodore Roosevelt -",
   },
   {
     quote:
       "Money is like gasoline during a road trip. You don't wnant to run out of gas on your trip, but you're not doing a tour of gas starions.",
-    author: "-Tim O'Reilly-",
+    author: "- Tim O'Reilly -",
   },
   {
     quote:
       "The only place where success comes before work is in the dictionary.",
-    author: "-Vidal Sassoon-",
+    author: "- Vidal Sassoon -",
   },
 ];
 
