6d36a0f12ae8b7fb9d7f3a3cb659776ac688d7c3
white-hun <121800818+white-hun@users.noreply.github.com>
2023-01-19 11:43:23 +0900

    Update MP

--- js/weather.js
+++ js/weather.js
@@ -25,15 +25,20 @@ function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const weatherIconImg = document.querySelector("#weather-icon");
+
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const weather = document.querySelector("#weather span:first-child");
+      const weatherIcon = data.weather[0].icon;
+      const weatherMain = data.weather[0].main;
+      const temp = data.main.temp;
+      const weatherIconAdrs = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+      const weather = document.querySelector("#weather span:nth-child(2)");
       const city = document.querySelector("#weather span:last-child");
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main}  /  ${data.main.temp.toFixed(
-        1
-      )}℃`;
+      weather.innerText = `${weatherMain}  /  ${temp.toFixed(1)}℃`;
+      weatherIconImg.setAttribute("src", weatherIconAdrs);
     });
 }
 function onGeoError() {
