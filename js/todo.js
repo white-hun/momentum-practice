98dc8d4ffdcb2d69b2473cd0940cc8a4fec21eb9
white <exwhite@naver.com>
2023-01-21 17:21:34 +0900

    Update MP

--- js/todo.js
+++ js/todo.js
@@ -7,7 +7,7 @@ const TODOS_KEY = "todos"; // todos의 key
 let toDos = []; // array
 
 function saveToDos() {
-  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); // array의 내용을 string 형태의 key:todos로 localstorage에 추가
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); // array의 내용을 string 형태의 key:todos로 localstorage에 추가;
 }
 
 function deleteToDo(event) {
@@ -45,7 +45,8 @@ function handleToDoSubmit(event) {
 }
 toDoForm.addEventListener("submit", handleToDoSubmit); // todo-form애 submit 시 함수 실행되는 event 추가
 
-const savedToDos = localStorage.getItem(TODOS_KEY); // array에 item을 추출
+const savedToDos = localStorage.getItem(TODOS_KEY); // array에 item을 추출;
+
 if (savedToDos !== null) {
   // 가져올게 있으면
   const parsedToDos = JSON.parse(savedToDos); // text 상태의 item들을 사용가능 인수로 변환;
